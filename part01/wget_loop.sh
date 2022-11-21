370da7bb9d87c3031dafd08f71a325d561c35fd1
ehsanghaffarii <ghafari.5000@gmail.com>
2022-11-21 09:05:07 +0330

    wget download loop

---
+++ part01/wget_loop.sh
@@ -0,0 +1,13 @@
+# /bin/bash
+
+# bash script for download a file in loop
+
+file_url="http://example/file.txt"
+
+COUNT=0
+
+while [ $COUNT -lt 10 ]; do
+  wget $file_url
+  sleep 120
+  COUNT=$((COUNT + 1))
+done
